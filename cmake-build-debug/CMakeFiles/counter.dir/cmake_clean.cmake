bb2819afa1ae76c4e25c90809e31f2ed8bb2e5cd
jacob_howard <jah0147@auburn.edu>
2022-07-21 17:41:01 -0500

    formatting

---
+++ cmake-build-debug/CMakeFiles/counter.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/counter.dir/main.cpp.obj"
+  "CMakeFiles/counter.dir/main.cpp.obj.d"
+  "counter.exe"
+  "counter.exe.manifest"
+  "counter.pdb"
+  "libcounter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/counter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
